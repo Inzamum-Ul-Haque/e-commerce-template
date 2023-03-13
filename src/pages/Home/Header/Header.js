e3efe64c6ec895d678c7c02b17e4ed239c2106c3
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2023-03-14 01:55:53 +0600

    commit

--- src/pages/Home/Header/Header.js
+++ src/pages/Home/Header/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Header.css";
 import winStorelogo from "../../../assets/logo/Vector 1.png";
+import { BsHeadphones, BsCart4, BsPerson } from "react-icons/bs";
+import { AiOutlineHeart } from "react-icons/ai";
 
 const Header = () => {
   return (
@@ -9,8 +11,35 @@ const Header = () => {
         <img src={winStorelogo} alt="" />
         <h3>store</h3>
       </div>
-      <div className="search-bar"></div>
-      <div className="header-icons"></div>
+      <div className="search-bar">
+        <div className="all-categories">
+          <select name="categories" id="category">
+            <option value="electronics">Electronics</option>
+            <option value="bags">Bags</option>
+            <option value="laptops">Laptops</option>
+            <option value="books">Books</option>
+            <option value="cloths">Cloths</option>
+            <option value="mobiles">Mobiles</option>
+          </select>
+        </div>
+        <div className="search-box">
+          <input type="text" placeholder="Search for a product" />
+        </div>
+      </div>
+      <div className="header-icons">
+        <div className="contact">
+          <p>Call us now</p>
+          <p>
+            <BsHeadphones /> +011 5827918
+          </p>
+          <p>Sign In</p>
+        </div>
+        <div>
+          <BsPerson />
+          <AiOutlineHeart />
+          <BsCart4 />
+        </div>
+      </div>
     </div>
   );
 };
