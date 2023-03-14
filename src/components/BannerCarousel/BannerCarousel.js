8aa7e09712e8e02fa04fb956a5efca662235a3e3
Inzamum-Ul-Haque <inzamum.inu@gmail.com>
2023-03-15 01:09:37 +0600

    commit

---
+++ src/components/BannerCarousel/BannerCarousel.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Carousel from "react-bootstrap/Carousel";
+
+const BannerCarousel = ({ slide }) => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img className="d-block w-100" src={slide.img} alt="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default BannerCarousel;
